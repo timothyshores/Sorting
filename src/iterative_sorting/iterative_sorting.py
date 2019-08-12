d7e4814b9c67eab8bddeed9579e38eb7f1ee46f3
Timothy Shores <timothymshores@gmail.com>
2019-08-12 16:42:40 -0500

    Complete selection sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,34 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
-    # loop through n-1 elements
+def selection_sort(arr):
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
-        # TO-DO: swap
-
-
-
-
+        current_num = arr[i]
+        count = 1
+        for j in arr[i + 1:]:
+            if current_num > j:
+                arr[i], arr[i + count] = arr[i + count], arr[i]
+            count += 1
     return arr
 
 
+print("\nSelection Sort")
+print(f"[]: {selection_sort([])}")
+print(f"[1]: {selection_sort([1])}")
+print(f"[1,2]: {selection_sort([1,2])}")
+print(f"[2,1]: {selection_sort([2,1])}")
+print(f"[1,2,3]: {selection_sort([1,2,3])}")
+print(f"[3,2,1]: {selection_sort([3,2,1])}")
+print(f"[1,2,3, 4]: {selection_sort([1,2,3,4])}")
+print(f"[4,3,2,1]: {selection_sort([4,3,2,1])}\n")
+
+
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+
+
+def bubble_sort(arr):
 
     return arr
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+def count_sort(arr, maximum=-1):
 
     return arr
